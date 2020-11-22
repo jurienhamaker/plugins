7760bdc329487fefbe074d6b9d4b0ba8e95352d0
Favna <contact@skyra.pw>
2020-11-22 15:00:08 +0000

    build: api build for 06918dd86102ff0105327556770542b620ae9043

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,9 +1,6 @@
 import mod from "./index.js";
 
 export default mod;
-export const __esModule = mod.__esModule;
-export const main = mod.main;
-export const kRoutePathCacheSymbol = mod.kRoutePathCacheSymbol;
 export const Api = mod.Api;
 export const ApiRequest = mod.ApiRequest;
 export const ApiResponse = mod.ApiResponse;
@@ -11,3 +8,6 @@ export const CookieStore = mod.CookieStore;
 export const Methods = mod.Methods;
 export const Server = mod.Server;
 export const Types = mod.Types;
+export const __esModule = mod.__esModule;
+export const kRoutePathCacheSymbol = mod.kRoutePathCacheSymbol;
+export const main = mod.main;
