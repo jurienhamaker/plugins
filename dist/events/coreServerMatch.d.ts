01d62bba862528804b90a1610b3f92e75b900513
Favna <contact@skyra.pw>
2020-12-24 13:51:15 +0000

    build: api build for 552b25170b0def63e745146ee48efccfd18fc3aa

--- dist/events/coreServerMatch.d.ts
+++ dist/events/coreServerMatch.d.ts
@@ -1,18 +1,9 @@
 import { Event, PieceContext } from '@sapphire/framework';
 import type { ApiRequest } from '../lib/structures/api/ApiRequest';
 import type { ApiResponse } from '../lib/structures/api/ApiResponse';
-import { Server, ServerOptions } from '../lib/structures/http/Server';
 import type { RouteMatch } from '../lib/structures/RouteStore';
 export declare class PluginEvent extends Event {
     constructor(context: PieceContext);
     run(request: ApiRequest, response: ApiResponse, match: RouteMatch): Promise<void>;
-}
-declare module 'discord.js' {
-    interface Client {
-        server: Server;
-    }
-    interface ClientOptions {
-        api?: ServerOptions;
-    }
 }
 //# sourceMappingURL=coreServerMatch.d.ts.map
